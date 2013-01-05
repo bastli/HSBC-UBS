2193d5868afc7c507f0c26bc10490c2f6f4e32fe
vagrant <vagrant@precise32.(none)>
2013-01-05 21:28:13 +0000

    Import Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HSBCUbs::Application.config.secret_token = '7d9cb3fbec52418d65d6e2a5da03c0ee020237d44c26d6f570dc553f5f5cc71dbf55a1467e691be5ff08b043a0edc123211f7d628aad4b2b070d046fea63e047'
